f7182710fc056d59e463569cac2703505b583034
Stutie <stutiebanerjie@gmail.com>
2026-02-24 21:02:03 -0500

    edited spacing in header

--- case001.sql
+++ case001.sql
@@ -1,5 +1,5 @@
 /** 
-    Case #001: The Vanishing Briefcase
+    Case # 001: The Vanishing Briefcase
     
     Description: Set in the gritty 1980s, a valuable briefcase has disappeared from the Blue Note Lounge. A witness reported that a man in a trench coat was seen fleeing the scene. Investigate the crime scene, review the list of suspects, and examine interview transcripts to reveal the culprit.
 **/
