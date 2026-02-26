ab58163f401ef829952cb1b11464eaef0195dd61
Stutie <stutiebanerjie@gmail.com>
2026-02-26 13:07:03 -0500

    update case001.sql

--- case-files/case001/case001.sql
+++ case-files/case001/case001.sql
@@ -1,15 +1,7 @@
-/** 
-    Case # 001: The Vanishing Briefcase
-    
-    Description: Set in the gritty 1980s, a valuable briefcase has disappeared from the Blue Note Lounge. A witness reported that a man in a trench coat was seen fleeing the scene. Investigate the crime scene, review the list of suspects, and examine interview transcripts to reveal the culprit.
-**/
-
--- Objective 1: Retrieve the correct crime scene details to gather the key clue.
+-- Objective 1
 SELECT * FROM crime_scene 
 WHERE location like '%Blue Note%'
-
-
--- Objectives 2 & 3: Identify the suspect whose profile matches the witness description & verify the suspect using their interview transcript.
+-- Objectives 2 & 3
 SELECT * FROM interviews 
 JOIN suspects
 ON interviews.suspect_id = suspects.id
