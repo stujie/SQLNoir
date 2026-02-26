52dd3fb0b4c65f1a82ed79d3f06dcc6bed796e0b
Stutie <stutiebanerjie@gmail.com>
2026-02-26 13:07:35 -0500

    update case002.sql

--- case-files/case002/case002.sql
+++ case-files/case002/case002.sql
@@ -1,16 +1,9 @@
-/** 
-    Case # 002: The Stolen Sound
-    
-    Description: In the neon glow of 1980s Los Angeles, the West Hollywood Records store was rocked by a daring theft. A prized vinyl record, worth over $10,000, vanished during a busy evening, leaving the store owner desperate for answers. Vaguely recalling the details, you know the incident occurred on July 15, 1983, at this famous store. Your task is to track down the thief and bring them to justice.
-**/
-
--- Objectives 1&2: Retrieve the crime scene report for the record theft using the known date and location and retrieve witness records linked to that crime scene to obtain their clues
+-- Objectives 1&2
 SELECT * FROM crime_scene c
 JOIN witnesses w
 ON c.id = w.crime_scene_id
 WHERE location like '%record%'
-
--- Objectives 3&4: Use the clues from the witnesses to find the suspect in the suspects table and retrieve the suspect's interview transcript to confirm the confession
+-- Objectives 3&4
 SELECT * FROM suspects s
 JOIN interviews i
 ON s.id = i.suspect_id
